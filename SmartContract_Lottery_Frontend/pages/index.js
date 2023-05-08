82e8fa32c43fd6cd7f986e2b77974bcf53231008
shivam6862 <shivam6862mau@gmail.com>
2023-05-09 03:36:13 +0530

    frontend

---
+++ SmartContract_Lottery_Frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our Smart Contract Lottery" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <div className="heading">Hi, I am shivam kumar</div>
+    </div>
+  );
+}
